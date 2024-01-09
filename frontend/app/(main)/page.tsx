d206d274ad2484d5d37e1914e10718a26fdf03ee
razcan <razvan.mustata@gmail.com>
2024-01-09 16:49:55 +0200

    added save ctr

--- frontend/app/(main)/page.tsx
+++ frontend/app/(main)/page.tsx
@@ -4,77 +4,72 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
+import { ProgressSpinner } from 'primereact/progressspinner';
+
 import {
-    QueryClient,
-    QueryClientProvider,
-    useQuery,
-  } from '@tanstack/react-query'
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+  useMutation,
+  useQueryClient
+} from '@tanstack/react-query'
 
 const queryClient = new QueryClient();
 
-const ContractListPage =  () => {
+const ContractListPage = () => {
 
- const [text, setText] = useState('');
- const [contracts, setContracts] = useState([]);
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div className="grid">
+        <div className="col-12">
+          <div className="card">
 
-//  const getdata = ()=> {
-//     const headers = {
-//         'Content-Type': 'application/json', // Example content type
-//         'Access-Control-Allow-Origin': '*', // Specify your allowed origin
-//       };
-
-//   axios.get('http://localhost:3000/contracts'
-//   , { headers }
-//   )
-//   .then(response => {
-//     // Handle the successful response
-//     setContracts(response.data);
-//     console.log('Response:', response.data);
-//   })
-//   .catch(error => {
-//     // Handle errors
-//     console.error('Error:', error);
-//   })
-// };
-
-    return (
-        <QueryClientProvider client={queryClient}>
-        <div className="grid">
-            <div className="col-12">
-                <div className="card">
-                    <h5>Empty Page x</h5>
-                    <Users />   
-                </div>
-            </div>
+            <Users />
+          </div>
         </div>
-        </QueryClientProvider>
-    );
+      </div>
+    </QueryClientProvider>
+  );
 };
 
 function Users() {
-    // 👇️ can use the useQuery hook here
-    const {isLoading, error, data} = useQuery({
-      queryKey: ['usersData'],
-      queryFn: () =>
-        fetch('http://localhost:3000/contracts').then(res => res.json()),
-    });
-  
-    if (isLoading) return 'Loading...';
-  
-    if (error) return 'An error has occurred: ' + error.message;
-  
-    return (
-      <div>
 
-                <DataTable value={data} stripedRows tableStyle={{ minWidth: '50rem' }}>
-                        <Column field="id" header="id"></Column>
-                        <Column field="number" header="number"></Column>
-                        <Column field="partner" header="partner"></Column>
-                        <Column field="status" header="status"></Column>
-                        <Column field="remarks" header="remarks"></Column>
-                    </DataTable>
-      </div>
-    );
-  }
+  const [selectedProduct, setSelectedProduct] = useState(null);
+  const [metaKey, setMetaKey] = useState(true);
+  console.log(selectedProduct)
+
+  // can use the useQuery hook here
+  const { isLoading, error, data } = useQuery({
+    queryKey: ['contractsData'],
+    queryFn: () =>
+      fetch('http://localhost:3000/contracts').then(res => res.json()),
+  });
+
+  if (isLoading) return (
+    <ProgressSpinner
+      style={{ width: "100px", height: "100px" }}
+      strokeWidth="4"
+      fill="var(--surface-ground)"
+      animationDuration=".5s"
+    />
+  );
+
+  if (error) return 'An error has occurred: ' + error.message;
+
+  return (
+    <div>
+
+      <DataTable value={data} stripedRows tableStyle={{ minWidth: '50rem' }} paginator rows={12} rowsPerPageOptions={[6, 12, 24, 48, 100]} sortMode="multiple"
+        selectionMode="single" selection={selectedProduct} onSelectionChange={(e) => setSelectedProduct(e.value)} dataKey="id" metaKeySelection={metaKey}
+      >
+        <Column field="id" header="id"></Column>
+        <Column field="number" header="number" sortable></Column>
+        <Column field="partner" header="partner" sortable></Column>
+        <Column field="status" header="status" sortable></Column>
+        <Column field="remarks" header="remarks" sortable></Column>
+      </DataTable>
+    </div>
+  );
+}
 
 export default ContractListPage;
--- frontend/app/(main)/uikit/formlayout/page.tsx
+++ frontend/app/(main)/uikit/formlayout/page.tsx
@@ -4,34 +4,69 @@ import { useRouter } from 'next/navigation';
 import React, { useState, useEffect, useMemo } from 'react';
 import { InputText } from 'primereact/inputtext';
 import { Button } from 'primereact/button';
-import { InputTextarea } from 'primereact/inputtextarea';
 import { Dropdown } from 'primereact/dropdown';
 import { TabMenu } from 'primereact/tabmenu';
+import { Calendar } from 'primereact/calendar';
+import { Editor } from 'primereact/editor';
+import axios from 'axios';
+
+import {
+    QueryClient,
+    QueryClientProvider,
+    useQuery,
+    useMutation,
+    useQueryClient
+} from '@tanstack/react-query'
+
 
 interface DropdownItem {
     name: string;
     code: string;
 }
 
+interface Contract {
+    number?: number,
+    type?: string,
+    partner?: string,
+    status?: string,
+    start?: Date,
+    end?: Date,
+    sign?: Date,
+    completion?: Date,
+    remarks?: string
+}
+
 const FormLayoutDemo = () => {
 
     const router = useRouter();
     const [dropdownItem, setDropdownItem] = useState<DropdownItem | null>(null);
+
+    const [number, setNumber] = useState(null);
+    const [partner, setPartner] = useState(null);
+    const [type, setType] = useState(null);
+    const [start, setStartDate] = useState(null);
+    const [end, setEndDate] = useState(null);
+    const [sign, setSignDate] = useState(null);
+    const [completion, setCompletionDate] = useState(null);
+    const [remarks, setRemarks] = useState(null);
+    const [status, setStatus] = useState(null);
+
     const dropdownItems: DropdownItem[] = useMemo(
         () => [
-            { name: 'Option 1', code: 'Option 1' },
-            { name: 'Option 2', code: 'Option 2' },
-            { name: 'Option 3', code: 'Option 3' }
+            { name: 'Activ', code: 'Activ' },
+            { name: 'Draft', code: 'Draft' },
+            { name: 'Anulat', code: 'Anulat' }
         ],
         []
     );
 
     const items = [
-        { label: 'Informatii generale', icon: 'pi pi-home' ,
-        command: () => {
-            router.push('/uikit/input');
-        }
-    },
+        {
+            label: 'Informatii generale', icon: 'pi pi-home',
+            command: () => {
+                router.push('/uikit/input');
+            }
+        },
         { label: 'Date Financiare', icon: 'pi pi-chart-line' },
         { label: 'Continut Contract', icon: 'pi pi-list' },
         { label: 'Document Atasate', icon: 'pi pi-inbox' },
@@ -42,53 +77,102 @@ const FormLayoutDemo = () => {
     ];
 
     useEffect(() => {
-        setDropdownItem(dropdownItems[1]);
+        setDropdownItem(dropdownItems['']);
     }, [dropdownItems]);
+
+    const saveContract = async () => {
+        // console.log(number, partner, start, end, completion, sign, type, remarks, status)
+        let addedContract: Contract = {
+            number: number,
+            type: type,
+            partner: partner,
+            status: status.name,
+            start: (start ? start.toISOString() : null),
+            end: (end ? end.toISOString() : null),
+            sign: (sign ? sign.toISOString() : null),
+            completion: (completion ? completion.toISOString() : null),
+            remarks: remarks
+        }
+
+        try {
+            const response = await axios.post('http://localhost:3000/contracts',
+                addedContract
+                // number, partner, start, end, completion, sign, type, remarks, status
+            );
+
+            console.log('Contract added:', response.data);
+        } catch (error) {
+            console.error('Error creating contract:', error);
+        }
+
+        // const queryClient = new QueryClient()
+
+        // const mutation = useMutation({
+        //     mutationFn: (newContract) => {
+        //         return axios.post('http://localhost:3000/contracts', newContract)
+        //     },
+        // })
+
+    }
 
     return (
         <div className="grid">
             <div className="col-12">
                 <div className="card">
-                    
+
                     <div className="field lg:col-12 xs:col-3 md:col-12">
-                         <TabMenu model={items} />
+                        <TabMenu model={items} />
                     </div>
 
                     <div className="p-fluid formgrid grid pt-2">
 
                         <div className="field col-12  md:col-3">
-                            <label htmlFor="firstname2">Firstname</label>
-                            <InputText id="firstname2" type="text" />
+                            <label htmlFor="number">Numar</label>
+                            <InputText id="number" keyfilter="int" type="text" value={number} onChange={(e) => setNumber(e.target.value)} />
                         </div>
                         <div className="field col-12 md:col-3">
-                            <label htmlFor="lastname2">Lastname</label>
-                            <InputText id="lastname2" type="text" />
+                            <label htmlFor="type">Tip</label>
+                            <InputText id="type" type="text" value={type} onChange={(e) => setType(e.target.value)} />
                         </div>
                         <div className="field col-12 md:col-3">
-                            <label htmlFor="lastname2">Lastname</label>
-                            <InputText id="lastname2" type="text" />
+                            <label htmlFor="partner">Partner</label>
+                            <InputText id="partner" type="text" value={partner} onChange={(e) => setPartner(e.target.value)} />
                         </div>
                         <div className="field col-12 md:col-3">
-                            <label htmlFor="lastname2">Lastname</label>
-                            <InputText id="lastname2" type="text" />
-                        </div>
-                        <div className="field col-12">
-                            <label htmlFor="address">Address</label>
-                            <InputTextarea id="address" rows={4} />
-                        </div>
-                        <div className="field col-12 md:col-6">
-                            <label htmlFor="city">City</label>
-                            <InputText id="city" type="text" />
+                            <label htmlFor="state">Stare</label>
+                            <Dropdown id="state" value={status} onChange={(e) => setStatus(e.value)} options={dropdownItems} optionLabel="name" placeholder="Select One"></Dropdown>
                         </div>
                         <div className="field col-12 md:col-3">
-                            <label htmlFor="state">State</label>
-                            <Dropdown id="state" value={dropdownItem} onChange={(e) => setDropdownItem(e.value)} options={dropdownItems} optionLabel="name" placeholder="Select One"></Dropdown>
+                            <label className="font-bold block mb-2">
+                                Start
+                            </label>
+                            <Calendar id="buttondisplay" value={start} onChange={(e) => setStartDate(e.value)} showIcon />
                         </div>
                         <div className="field col-12 md:col-3">
-                            <label htmlFor="zip">Zip</label>
-                            <InputText id="zip" type="text" />
+                            <label className="font-bold block mb-2">
+                                Final
+                            </label>
+                            <Calendar id="buttondisplay" value={end} onChange={(e) => setEndDate(e.value)} showIcon />
+                        </div>
+                        <div className="field col-12 md:col-3">
+                            <label className="font-bold block mb-2">
+                                Semnat
+                            </label>
+                            <Calendar id="buttondisplay" value={sign} onChange={(e) => setSignDate(e.value)} showIcon />
+                        </div>
+                        <div className="field col-12 md:col-3">
+                            <label className="font-bold block mb-2">
+                                Inchis la
+                            </label>
+                            <Calendar id="buttondisplay" value={completion} onChange={(e) => setCompletionDate(e.value)} showIcon />
+                        </div>
+                        <div className="field col-12 md:col-12">
+                            <Editor value={remarks} onTextChange={(e) => setRemarks(e.htmlValue)}
+                                className='max-w-screen' style={{ height: '320px' }}
+                            />
                         </div>
                     </div>
+                    <Button label="Salveaza" onClick={saveContract} />
                 </div>
             </div>
         </div>
