aff24d2cb80fe0a9629c413cd3c78a8ddc2558f3
razcan <razvan.mustata@gmail.com>
2024-01-24 19:11:50 +0200

    frontend: Wed Jan 24 19:11:50 EET 2024

--- frontend/app/(main)/uikit/lookups/addpartner/address.tsx
+++ frontend/app/(main)/uikit/lookups/addpartner/address.tsx
@@ -141,7 +141,7 @@ const PartnerAddress = ({ params, setAddressIndex, setAddressChild }: any) => {
         };
 
         //verif daca id exista in array
-        if (selectedAddress.id) { console.log("se editeaza") } else { console.log("se adauga") }
+        // if (selectedAddress.id) { console.log("se editeaza") } else { console.log("se adauga") }
         //myAddressArray.map((address) => { console.log("iduri", address.id) })
 
         const newAddressToBeSent: any = {
@@ -165,25 +165,58 @@ const PartnerAddress = ({ params, setAddressIndex, setAddressChild }: any) => {
 
 
         // Update the state by spreading the existing array and adding the new item
-        setMyAddressArray((prevArray) => [...prevArray, newAddress]);
+        if (!selectedAddress.id) {
+            console.log("se adauga")
+            setMyAddressArray((prevArray) => [...prevArray, newAddress]);
+            setAddressChild((prevArray) => [...prevArray, newAddressToBeSent]);
 
-        setAddressChild((prevArray) => [...prevArray, newAddressToBeSent]);
+            setselectedAddress('')
+            setAddressName('')
+            setCountry('');
+            setCounty('');
+            setSelectedAddressType('');
+            setCity('')
+            setStreet('')
+            setNumber('')
+            setPostalCode('')
+            setSelectedStatus(false)
+            setSelectedDefault(false)
+            setAggregate(true)
+            setCompleteAddress('')
 
-        setselectedAddress('')
-        setAddressName('')
-        setCountry('');
-        setCounty('');
-        setSelectedAddressType('');
-        setCity('')
-        setStreet('')
-        setNumber('')
-        setPostalCode('')
-        setSelectedStatus(false)
-        setSelectedDefault(false)
-        setAggregate(true)
-        setCompleteAddress('')
+            setVisibleAddress(false)
+        }
+        else {
+            console.log("se editeaza")
 
-        setVisibleAddress(false)
+            let addressindex: number = myAddressArray.findIndex(address => address.id === selectedAddress.id);
+            console.log("index de editat ", addressindex);
+            myAddressArray[addressindex] = newAddress;
+
+            setselectedAddress('')
+            setAddressName('')
+            setCountry('');
+            setCounty('');
+            setSelectedAddressType('');
+            setCity('')
+            setStreet('')
+            setNumber('')
+            setPostalCode('')
+            setSelectedStatus(false)
+            setSelectedDefault(false)
+            setAggregate(true)
+            setCompleteAddress('')
+
+            setVisibleAddress(false)
+
+            // if (addressindex !== -1) {
+            //     console.log(`Address with id ${selectedAddress.id} found at index ${addressindex}`);
+            //     myAddressArray.splice(addressindex, 1);
+
+            // }
+        }
+
+
 
     };
 
